a757f5ecbe0c198fde5a7334edcab8920d233f24
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-10-22 11:43:02 -0400

    solved w/ regex

---
+++ spinalTapCase/spinalTapCase.js
@@ -0,0 +1,33 @@
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes
+//can this be done w/o regex?
+//regex:
+function spinalCase(str) {
+  //this takes care of the test cases w/o regex
+  //going to scrap it for now and try regex
+  // var lowerCase = str.toLowerCase();
+  // var split = lowerCase.split(" ");
+  // var join = split.join("-");
+  // console.log(join);
+
+  //second attempt w/ regex and replace() - in fcc documentaion
+
+  // match whitespace characters = \s_ and look in array []
+  //g, = globallly
+  //the below code will pass all the test cases with spaces already
+
+  // return str.replace(/[\s_]/g, '-').toLowerCase();
+
+  //now the test cases with camelCase must be seperated
+  //capturing groups can help this
+  //[a-z] and [A-Z] stand for the two capturing groups
+  //match any lower case letter or uppercase letter
+  //$1-$2 represent the same two captuing groups but this time joined by a '-'
+  return str
+    .replace(/[\s_]/g, "-")
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .toLowerCase();
+
+  return str;
+}
+
+spinalCase("This Is Spinal Tap");
