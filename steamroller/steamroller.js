5adb81a055600ab8cc7ed3fa93505c995c5f9075
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-11-04 10:23:23 -0500

    solved w/ arr.flat()

---
+++ steamroller/steamroller.js
@@ -0,0 +1,21 @@
+//arr.flat() will be very useful for this algorithm
+//spent some time trying to loop thru and flatten each iteration of arr
+//then I came acrosss the arr.flat(infinity)
+//done
+
+function steamrollArray(arr) {
+
+  for (var i = 0; i < arr.length; i++) {
+
+    var flatten = arr.flat(Infinity);
+    console.log(flatten)
+
+    // var flatten = arr[i].flat()
+    // console.log(flatten)
+
+  }
+  // I'm a steamroller, baby
+  return flatten;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
