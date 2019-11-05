02428051b983a16d62bbace4809c56beb0ded6c7
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-11-05 11:24:16 -0500

    solved with shift()

---
+++ dropIt/dropIt.js
@@ -0,0 +1,19 @@
+//run a loop, probably a while loop
+//run the passed in function until it returns true
+//shift() would be helpful here
+
+function dropElements(arr, func) {
+  //while the function is false in arr index 0
+  var array = [];
+  while (!func(arr[0])) {
+    //use shift to drop the index's off until it finds the right one
+    arr.shift();
+  }
+
+  // Drop them elements.
+  return arr;
+}
+
+dropElements([1, 2, 3], function (n) {
+  return n < 3;
+});
